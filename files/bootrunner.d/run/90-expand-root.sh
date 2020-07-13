d91bcc77599a9b4b2495ed3f1195edd35d1e6039
Sam Hofius <sam@samhofi.us>
2020-07-13 02:04:45 -0400

    Allow get_wpa and bootrunner to run concurrently, but make sure the expand-root script isn't run until get_wpa finishes

--- files/bootrunner.d/run/90-expand-root.sh
+++ files/bootrunner.d/run/90-expand-root.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# make sure get_wpa service has completed so we don't rip the filesystem
+# out while it's in the middle of copying one of its files
+WAIT=0
+while [ "$(systemctl is-failed get_wpa.service)" != "inactive" ]; do
+    WAIT=1
+    echo "wait for get_wpa to finish"
+    sleep 1
+done
+test "${WAIT}" == "1" && echo "done waiting for get_wpa to finish"
+
 # resize root partition
 echo -e "d\n3\nn\np\n3\n\n\nn\nw\n" | fdisk /dev/mmcblk0
 
@@ -8,5 +18,5 @@ cat <<EOF > /etc/bootrunner.d/run/00-resize2fs.sh
 resize2fs /dev/mmcblk0p3
 EOF
 
-chmod +x /etc/bootrunner.d/run/00-resize2fs.sh
+mv /etc/bootrunner.d/done/00-stop-resolved.sh /etc/bootrunner.d/run/
 touch /etc/bootrunner.d/reboot
